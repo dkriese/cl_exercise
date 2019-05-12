9e116ed3b0879160048810d5d0846c4750366ed8
Diana K <dkriese@gmail.com>
2019-05-12 17:01:03 -0600

    Adding Vue production settings

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,11 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 Vue.component('people-component', require('./components/PeopleComponent.vue').default);
-
+if (process.env.MIX_ENV_MODE === 'production') {
+    Vue.config.devtools = false;
+    Vue.config.debug = false;
+    Vue.config.silent = true;
+}
 
 const app = new Vue({
     el: '#app',
