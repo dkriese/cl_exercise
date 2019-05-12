e7758a4a05a2506f318aa0ddd07af55b8e0fb0dc
Diana K <dkriese@gmail.com>
2019-05-12 16:58:54 -0600

    Added user friendly error messages to request validation class

--- app/Http/Requests/StorePeoplePost.php
+++ app/Http/Requests/StorePeoplePost.php
@@ -23,13 +23,23 @@ class StorePeoplePost extends FormRequest
      */
     public function rules()
     {
-        //dd(request()->all());
         return [
             'data.*.first_name' => 'required|max:255',
             'data.*.last_name' => 'required|max:255',
             'data.*.email' => 'required|email',
             'data.*.age' => 'required|numeric|min:0|max:125',
             'data.*.secret' => 'required|max:255'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'data.*.first_name.required' => 'First Name is required',
+            'data.*.last_name.required' => 'Last Name is required',
+            'data.*.email.required' => 'Email is required',
+            'data.*.age.required' => 'Age is required',
+            'data.*.secret.required' => 'Secret is require'
         ];
     }
 }
